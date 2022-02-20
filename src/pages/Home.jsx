7a3c201edf2e200ff808e677e512a75a8c4b03ee
ermias kidane <ermiaskidanegebre@gmail.com>
2022-02-20 22:38:07 +0000

    added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResult from '../components/users/UserResult'
+
+function Home() {
+  return (
+    <>
+      {/* Search user */}
+      <UserResult />
+    </>
+  )
+}
+
+export default Home
